f28174e8e9228d54cd476da7b25d51f100a8df04
doanmade <43007082+doanmade@users.noreply.github.com>
2019-06-14 11:55:14 -0500

    able to add smurfs but page has to refresh - working on that and breaking it into two pages

--- village/src/components/SmurfForm.js
+++ village/src/components/SmurfForm.js
@@ -1,27 +1,39 @@
-import React, { Component } from 'react';
+import axios from "axios";
+import React, { Component } from "react";
 
 class SmurfForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      name: '',
-      age: '',
-      height: ''
+      name: "",
+      age: "",
+      height: ""
     };
   }
 
   addSmurf = event => {
     event.preventDefault();
     // add code to create the smurf using the api
-
-    this.setState({
-      name: '',
-      age: '',
-      height: ''
-    });
-  }
+    axios
+      .post("http://localhost:3333/smurfs", {
+        name: this.state.name,
+        age: this.state.age,
+        height: this.state.height
+      })
+      .then(() => {
+        this.setState({
+          name: "",
+          age: "",
+          height: ""
+        });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
 
   handleInputChange = e => {
+    e.preventDefault();
     this.setState({ [e.target.name]: e.target.value });
   };
 
